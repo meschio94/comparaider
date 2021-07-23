5050e3714629699158cd04de52370dc59dfbaf67
meschio94 <meschio94@gmail.com>
2021-07-23 21:25:15 +0200

    admin glider/maker

--- showcase/admin.py
+++ showcase/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from showcase.models import Maker
+from showcase.models import Glider
 
 # Register your models here.
+admin.site.register(Maker)
+admin.site.register(Glider)
